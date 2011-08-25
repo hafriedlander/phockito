69768836c36de05268499138a364af7adf59b3c8
Hamish Friedlander <hamish@silverstripe.com>
2011-08-25 15:37:32 +1200

    BUGFIX: When added to SilverStripe, PhockitoSilverStripeTest.php would override the actual manifest if included.

--- tests/PhockitoSilverStripeTest.php
+++ tests/PhockitoSilverStripeTest.php
@@ -1,8 +1,5 @@
 <?php
 
-global $_ALL_CLASSES;
-$_ALL_CLASSES = array();
-
 require_once(dirname(dirname(__FILE__)) . '/Phockito.php');
 
 interface PhockitoSilverStripeTest_Interface {
@@ -17,7 +14,8 @@ class PhockitoSilverStripeTest_Child extends PhockitoSilverStripeTest_Base {
 class PhockitoSilverStripeTest_Implementor implements PhockitoSilverStripeTest_Interface {
 }
 
-$_ALL_CLASSES = array(
+global $_PSST_ALL_CLASSES;
+$_PSST_ALL_CLASSES = array(
 	'parents' => array(
 		'PhockitoSilverStripeTest_Base' => array(),
 		'PhockitoSilverStripeTest_Child' => array('PhockitoSilverStripeTest_Base' => 'PhockitoSilverStripeTest_Base'),
@@ -28,41 +26,49 @@ $_ALL_CLASSES = array(
 	)
 );
 
-
 class PhockitoSilverStripeTest extends PHPUnit_Framework_TestCase {
 
+	static $orig_type_registrar;
+	static $orig_all_classes;
+
 	function setUp() {
+		self::$orig_type_registrar = Phockito::$type_registrar;
+		
 		require_once(dirname(dirname(__FILE__)) . '/PhockitoSilverStripe.php');
 		Phockito::$type_registrar = 'PhockitoSilverStripe';
+		
+		self::$orig_all_classes = PhockitoSilverStripe::$_all_classes;
+		PhockitoSilverStripe::$_all_classes = '_PSST_ALL_CLASSES';
 	}
 
 	function tearDown() {
-		Phockito::$type_registrar = null;
+		PhockitoSilverStripe::$_all_classes = self::$orig_all_classes;
+		Phockito::$type_registrar = self::$orig_type_registrar;
 	}
 
 	function testParentInjection() {
-		global $_ALL_CLASSES;
+		global $_PSST_ALL_CLASSES;
 
 		$class = Phockito::spy_class('PhockitoSilverStripeTest_Child');
 
-		$this->assertEquals($_ALL_CLASSES['parents'][$class], array(
+		$this->assertEquals($_PSST_ALL_CLASSES['parents'][$class], array(
 			'PhockitoSilverStripeTest_Base' => 'PhockitoSilverStripeTest_Base',
 			'PhockitoSilverStripeTest_Child' => 'PhockitoSilverStripeTest_Child'
 		));
 	}
 
 	function testImplementsInjectionOfAClassDouble() {
-		global $_ALL_CLASSES;
+		global $_PSST_ALL_CLASSES;
 
 		$class = Phockito::spy_class('PhockitoSilverStripeTest_Implementor');
-		$this->assertTrue(array_key_exists($class, $_ALL_CLASSES['implementors']['PhockitoSilverStripeTest_Interface']));
+		$this->assertTrue(array_key_exists($class, $_PSST_ALL_CLASSES['implementors']['PhockitoSilverStripeTest_Interface']));
 	}
 
 	function testImplementsInjectionOfAnInterfaceDouble() {
-		global $_ALL_CLASSES;
+		global $_PSST_ALL_CLASSES;
 
 		$class = Phockito::mock_class('PhockitoSilverStripeTest_Interface');
-		$this->assertTrue(array_key_exists($class, $_ALL_CLASSES['implementors']['PhockitoSilverStripeTest_Interface']));
+		$this->assertTrue(array_key_exists($class, $_PSST_ALL_CLASSES['implementors']['PhockitoSilverStripeTest_Interface']));
 	}
 
 }
